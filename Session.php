ddd13ebf79e65713056cb6d3e69acb5f80508e90
sunjiang <2051787083@qq.com>
2021-01-15 18:10:53 +0800

    第二次修改

--- Session.php
+++ Session.php
@@ -4,4 +4,4 @@ unset($_SESSION['user']);
 setcookie('user_names','',time()-1);
 setcookie('user_ids','',time()-1);
 echo "正在退出";
-header('Refresh:2,url=login.html');
+header('Refresh:2,url=login1.html');
